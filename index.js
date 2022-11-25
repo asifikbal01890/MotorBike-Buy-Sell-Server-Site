6d33e56f8a794cdbe8b709b7a45c633af3b804a8
asifilbal01890 <asifikbal01890@gmail.com>
2022-11-25 20:44:46 +0600

    categories api impliment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -9,9 +10,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vh3xqbm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
     try {
+        const categoryCollection = client.db('motorbike').collection('categories');
         
+        app.get('/categories', async(req, res) => {
+            const query = {};
+            const option = await categoryCollection.find(query).toArray(); 
+            res.send(option);
+        })
+
     } 
     finally {
         
