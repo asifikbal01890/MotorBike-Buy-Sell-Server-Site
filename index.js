b5afe932a6fcf9a029d0be5065d3d6c82a87add9
asifilbal01890 <asifikbal01890@gmail.com>
2022-11-27 01:21:59 +0600

    jwt implement

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const { query } = require('express');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -24,47 +24,58 @@ async function run() {
         const usersCollection = client.db('motorbike').collection('users');
         // const standardBikeCollection = client.db('motorbike').collection('standardBike');
         // const cruiserBikeCollection = client.db('motorbike').collection('cruiserBike');
-        
-        app.get('/categories', async(req, res) => {
+
+        app.get('/categories', async (req, res) => {
             const query = {};
-            const categoryList = await categoryCollection.find(query).toArray(); 
+            const categoryList = await categoryCollection.find(query).toArray();
             res.send(categoryList);
         })
 
-        app.get('/categories/:id', async(req, res) => {
+        app.get('/categories/:id', async (req, res) => {
             id = req.params.id;
             console.log
-            const query = { _id : ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const categoryList = await categoryCollection.findOne(query);
             res.send(categoryList);
         })
-        
-        app.get('/bikes', async(req, res) => {
+
+        app.get('/bikes', async (req, res) => {
             const id = req.query.id
-            const query = {id: id};
-            const bikesList = await bikeCollection.find(query).toArray(); 
+            const query = { id: id };
+            const bikesList = await bikeCollection.find(query).toArray();
             res.send(bikesList);
         })
 
-        app.post('/bookings', async(req, res) =>{
+        app.post('/bookings', async (req, res) => {
             const booking = req.body;
             const query = {};
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
 
-        app.post('/users', async(req, res) =>{
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send({ accessToken: 'you are not authorized' })
+        })
+
+        app.put('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result);
-          })
-    } 
+        })
+    }
     finally {
-        
+
     }
 }
 run().catch(console.log);
-app.get('/', async(req, res) =>{
+app.get('/', async (req, res) => {
     res.send('server is running');
 })
 
