dc1b057b0001de4480971155f8271d3347616fad
asifilbal01890 <asifikbal01890@gmail.com>
2022-11-27 18:39:23 +0600

    buyer verification implement

--- index.js
+++ index.js
@@ -101,6 +101,13 @@ async function run() {
             const user = await usersCollection.findOne(query);
             res.send({isSeller: user?.role === true});
         })
+
+        app.get('/users/buyer/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {email}
+            const user = await usersCollection.findOne(query);
+            res.send({isBuyer: user?.role === false});
+        })
     }
     finally {
 
